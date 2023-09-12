190c543303227967445ebf94a5f1c759f0809a90
Victor Payno <vpayno@users.noreply.github.com>
2023-09-11 21:56:22 -0700

    feat: add tokio web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use std::net::SocketAddr; /* 1 */
+
+#[tokio::main] /* 2 */
+async fn main() {
+    let app = Router::new(); /* 3 */
+
+    let addr = SocketAddr::new([0, 0, 0, 0].into(), 3000); /* 4 */
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap(); /* 5 */
 }
