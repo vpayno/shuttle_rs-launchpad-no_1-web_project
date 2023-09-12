2830f230221364489bbfc1127c3e32343f88f08f
Victor Payno <vpayno@users.noreply.github.com>
2023-09-11 22:02:46 -0700

    feat: add hello_world route

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use axum::Router;
-use std::net::SocketAddr; /* 1 */
+use axum::{response::IntoResponse, routing::get, Router};
+use std::net::SocketAddr;
 
-#[tokio::main] /* 2 */
+async fn hello_world() -> impl IntoResponse {
+    "Hello World"
+}
+
+#[tokio::main]
 async fn main() {
-    let app = Router::new(); /* 3 */
+    let app = Router::new().route("/", get(hello_world));
 
-    let addr = SocketAddr::new([0, 0, 0, 0].into(), 3000); /* 4 */
+    let addr = SocketAddr::new([0, 0, 0, 0].into(), 3000);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
-        .unwrap(); /* 5 */
+        .unwrap();
 }
